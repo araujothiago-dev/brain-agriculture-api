69bab6cf3a186d48304d011bfde95eb4976363f8
thiago.machado <thiago.machado@seati.ma.gov.br>
2025-06-12 18:40:01 -0300

    feat: add useguards para controle de perfil e permissoes

--- src/dashboard/controller/dashboard.controller.ts
+++ src/dashboard/controller/dashboard.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ResponseGeneric } from 'src/utils/response.generic';
 import { DashboardService } from '../service/dashboard.service';
+import PermissionGuard from 'src/auth/guards/permission.guard';
+import DashboardPermission from '../enum/dashboardPermission.enum';
 
+@ApiBearerAuth('access_token')
 @ApiTags('dashboard')
 @ApiResponse({type: ResponseGeneric})
 @Controller('dashboard')
@@ -10,11 +13,13 @@ export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get('admin')
+  @UseGuards(PermissionGuard(DashboardPermission.LER_DASHBOARD, false))
   async findAllAdmin() {
     return await this.dashboardService.findAllAdmin();
   }
 
   @Get('produtor/:idProdutor')
+  @UseGuards(PermissionGuard(DashboardPermission.LER_DASHBOARD, false))
   async findAllProdutor(@Query('idProdutor') idProdutor: string) {
     return await this.dashboardService.findAllProdutor(idProdutor);
   }
