66a62fb67fb8e0642d2d215bfa20b1ef64273b36
thiago.machado <thiago.machado@seati.ma.gov.br>
2025-06-12 18:44:11 -0300

    feat: add execução de seeders

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
+import { seedSafra } from './config/seeders/seedSafra';
+import { seedCultura } from './config/seeders/seedCultura';
+import { seedProdutoresPropriedades } from './config/seeders/seedProdutoresPropriedades';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const dataSource = app.get(DataSource);
+
+  if (process.env.NODE_ENV !== 'PRODUCTION') {
+    await seedSafra(dataSource);
+    await seedCultura(dataSource);
+    await seedProdutoresPropriedades(dataSource);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
