992e04058a0aa57575a52f018bf014786c8dbf90
thiago.machado <thiago.machado@seati.ma.gov.br>
2025-06-26 10:56:28 -0300

    feat: configura swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DataSource } from 'typeorm';
 import { AppModule } from './app.module';
 import { seeds } from './config/seeders/seed';
 import { HttpExceptionFilter } from './utils/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -23,8 +24,26 @@ async function bootstrap() {
     }),
   );
 
-  if (process.env.NODE_ENV !== 'PRODUCTION') {
+  if (process.env.NODE_ENV === 'DEV') {
     await seeds(dataSource);
+
+    const config = new DocumentBuilder()
+      .setTitle('brain-agriculture.api')
+      .setDescription('API para agricultura de precisão')
+      .setVersion('1.0')
+      .setContact('AGRICULTURA', 'https://brain-agriculture.com.br', 'adminagriculturadeprecisao@brain-agriculture.com.br')
+      .setLicense('Apache 2.0', 'https://www.apache.org/licenses/LICENSE-2.0.html')
+      .addBearerAuth({
+        type: 'http',
+        bearerFormat: 'JWT',
+        scheme: 'Bearer',
+        in: 'header'
+      }, 'access-token')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
   }
 
   await app.listen(Number(process.env.PORT));
